5b8efcaa906fbc0a5a166a32c033cf10eedf0673
Elohina Guevara <elohinaguevara@gmail.com>
2017-10-02 03:03:50 -0430

    Add app.component and data.service tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,20 +1,52 @@
 import { TestBed, async } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MockBackend } from '@angular/http/testing';
 
 import { AppComponent } from './app.component';
+import { DataService } from './shared/data.service';
 import { HabitantsModule } from './habitants/habitants.module';
+import { MatSelectModule,
+  MatInputModule,
+  MatCardModule,
+  MatGridListModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatPaginatorModule,
+  MatChipsModule } from '@angular/material';
 
 describe('AppComponent', () => {
+  class MockService {
+    getData() {
+      return {"test": [{id:1, name: "name"}]};
+    }
+  }
+
+  let mockService;
   beforeEach(async(() => {
+    mockService = new MockService();
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [HabitantsModule, CommonModule, FormsModule, MatSelectModule,
+        MatInputModule,
+        MatCardModule,
+        MatGridListModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatPaginatorModule,
+        MatChipsModule
+      ],
+      providers: [
+        {provide: DataService, useValue: mockService}
+      ]
     }).compileComponents();
   }));
 
-  // it('should create the app', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app).toBeTruthy();
-  // }));
+  it('should create the app', async(() => {
+     const fixture = TestBed.createComponent(AppComponent);
+     const app = fixture.debugElement.componentInstance;
+     expect(app).toBeTruthy();
+  }));
 });
--- src/app/shared/data.service.spec.ts
+++ src/app/shared/data.service.spec.ts
@@ -1,50 +1,63 @@
 import { TestBed, async, inject } from '@angular/core/testing';
-
-import { DataService } from './data.service';
 import { MockBackend } from '@angular/http/testing';
-
 import {
-    HttpModule,
-    Http,
-    Response,
-    ResponseOptions,
-    XHRBackend,
-    ConnectionBackend,
-    RequestOptions
+  HttpModule,
+  Http,
+  Response,
+  ResponseOptions,
+  XHRBackend
 } from '@angular/http';
 
+import { DataService } from './data.service';
+import { DataStore } from './data-store.service';
+import { AppConfig } from "../app.config";
+
 describe('DataService', () => {
-    let service;
 
-    beforeEach(() => TestBed.configureTestingModule({
-        providers: [ DataService, Http, ConnectionBackend, RequestOptions, XHRBackend, HttpModule ]
-      }));
+  class MockConfig {
+    static API_URL = 'https://test/data.json';
+  }
 
-      beforeEach(inject([DataService], s => {
-        service = s;
-      }));
+  let mockConfig;
+  beforeEach(async(() => {
+    mockConfig =  new MockConfig();
+    TestBed.configureTestingModule({
+      declarations: [
 
-      it('should return an array of habitants from Brastlewark', inject([XHRBackend], (mockBackend) => {
-        const mockResponse = {
-            data: [
-              { id: 0, name: 'Video 0' },
-              { id: 1, name: 'Video 1' },
-              { id: 2, name: 'Video 2' },
-              { id: 3, name: 'Video 3' },
-            ]
-        };
-        mockBackend.connections.subscribe(
-            (connection) => {
-                connection.mockRespond(new Response(new ResponseOptions({
-                    body: JSON.stringify(mockResponse)
-                })));
-            }
-        );
-        mockBackend.getData().subscribe(
-            (habitants) => {
-                expect(habitants.length).toBe(4);
-                expect(habitants[0].id).toEqual(0);
-            }
-        );
-      }));
+      ],
+      imports: [HttpModule],
+      providers: [
+        {provide: XHRBackend, useClass: MockBackend},
+        {provide: AppConfig, useValue: mockConfig},
+        DataService,
+        DataStore
+      ]
+    }).compileComponents();
+  }));
+
+  it('should initialize cities and habitants', inject([XHRBackend, DataService, DataStore], (mockBackend, dataService, dataStore) => {
+    const mockResponse = {
+      "city1": [
+        { id: 0, name: 'Person 0' },
+        { id: 1, name: 'Person 1' },
+        { id: 2, name: 'Person 2' },
+        { id: 3, name: 'Person 3' },
+      ]
+    };
+
+    mockBackend.connections.subscribe((connection) => {
+      connection.mockRespond(new Response(new ResponseOptions({
+        body: JSON.stringify(mockResponse)
+      })));
+    });
+
+    dataService.getData().subscribe(
+        (data: any) => {
+          expect(dataStore._cities).toEqual(['city1']);
+          let habitants = dataStore.getCountriesHabitants('city1');
+          expect(habitants).not.toBe(null);
+          expect(habitants[0].name).toBe('Person 0');
+        }
+    );
+  }));
 });
